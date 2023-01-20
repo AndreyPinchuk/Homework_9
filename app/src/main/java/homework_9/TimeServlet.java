1913dd9f43e6e46e4c8c1b18f0f144cb5c18ef54
AndreyPinchuk <44933416+AndreyPinchuk@users.noreply.github.com>
2023-01-20 22:42:44 +0200

    new

---
+++ app/src/main/java/homework_9/TimeServlet.java
@@ -0,0 +1,83 @@
+package homework_9;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
+@WebServlet(value = "/time")
+public class TimeServlet extends HttpServlet {
+    private TemplateEngine engine;
+
+    @Override
+    public void init() throws ServletException {
+        engine = new TemplateEngine();
+
+        FileTemplateResolver resolver = new FileTemplateResolver();
+        resolver.setPrefix("C:\\IdeaProjects\\Homework_9\\templates\\");
+//        resolver.setPrefix("./templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML5");
+        resolver.setOrder(engine.getTemplateResolvers().size());
+        resolver.setCacheable(false);
+        engine.addTemplateResolver(resolver);
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html; charset=utf-8");
+
+        StringBuilder timezone = new StringBuilder(parsTimeParam(req));
+
+        if(req.getParameterMap().containsKey("timezone")){
+
+            resp.addCookie(new Cookie("lastTimezone",parsTimeParam(req)));
+
+        } else {
+            Cookie[] cookies =req.getCookies();
+            if(cookies != null){
+                for (Cookie cookie : cookies) {
+                    timezone.delete(0,timezone.length());
+                    timezone.append(cookie.getValue());
+                }
+            }
+        }
+
+        Context simpleContext = new Context(
+                req.getLocale(),
+                Map.of(
+                        "dateAndTime",
+                        getTime(timezone.toString())
+                )
+        );
+
+        engine.process("test",simpleContext,resp.getWriter());
+        resp.getWriter().close();
+    }
+    private String parsTimeParam(HttpServletRequest request){
+        if(request.getParameterMap().containsKey("timezone")){
+            return request.getParameter("timezone").replace(' ','+');
+        }
+        return "UTC";
+    }
+
+    public String getTime(String timezone){
+        String b =
+                ZonedDateTime.now(ZoneId.of(timezone))
+                        .format(DateTimeFormatter
+                                .ofPattern("dd-MM-yyyy HH:mm:ss z"));
+        return b;
+    }
+}
---
+++ app/src/main/java/homework_9/TimezoneValidateFilter.java
@@ -0,0 +1,52 @@
+package homework_9;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(value = "/time")
+public class TimezoneValidateFilter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest req,
+                            HttpServletResponse res,
+                            FilterChain chain) throws IOException, ServletException {
+
+        String timezone = parsTimeParam(req);
+
+        if(isValidTimeZone(timezone)){
+            chain.doFilter(req, res);
+
+        } else {
+            res.setStatus(400);
+            res.setContentType("text/html; charset=utf-8");
+            res.getWriter().write("<h1>Invalid timezone</h1>");
+            res.getWriter().close();
+        }
+    }
+    private boolean isValidTimeZone(String st){
+        String num = st
+                .replace("UTC+"," ")
+                .replace("UTC "," ")
+                .replace("UTC-"," ");
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i <13 ; i++) {
+            sb.append(" ").append(i);
+
+            if(num.equals(sb.toString())) return true;
+
+            sb.delete(0,sb.length());
+        }
+        return false;
+    }
+    private String parsTimeParam(HttpServletRequest request){
+        if(request.getParameterMap().containsKey("timezone")){
+            return request.getParameter("timezone");
+        }
+        return "UTC 0";
+    }
+}
